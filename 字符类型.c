884a05ac03e2dc67dcdf0774bffb1738ad8d0bff
BinaryHack-cpu <17613780060@163.com>
2025-05-20 00:02:36 +0800

    c语言的数据类型运算
    c语言的数据类型运算，可以详细看看

---
+++ 字符类型.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	// 1，定义char数据类型的变量
+//	// char字符的取值范围是ASCII码表的所有内容 windows占用1个字节
+//	char c1 = 'a';
+//	printf("%c\n", c1);
+//
+//	char c2 = 'A';
+//	printf("%c\n", c2);
+//
+//	char c3 = '.';
+//	printf("%c\n", c3);
+//
+//
+//	//2，利用sizeof测量占用多少个字节
+//	printf("%zu\n", sizeof(char));
+//
+//	return 0;
+//}
